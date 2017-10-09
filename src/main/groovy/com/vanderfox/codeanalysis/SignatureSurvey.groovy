e15676c8beef1387e4073bc6ad589c67481ff635
lfox <lee.h.fox@gmail.com>
2017-10-08 23:09:28 -0500

    recurses directories now

--- src/main/groovy/com/vanderfox/codeanalysis/SignatureSurvey.groovy
+++ src/main/groovy/com/vanderfox/codeanalysis/SignatureSurvey.groovy
@@ -6,18 +6,42 @@ package com.vanderfox.codeanalysis
 class SignatureSurvey {
 
     static void main(String[] args) {
-        File file = new File(args[0])
-        def lines = file.readLines()
+        if (args.length < 1) {
+            print "usage:  SignatureSurvey <file> <list of characters separated by spaces>"
+            return
+        }
+        recurseDirectory(new File(args[0]), args)
+
+    }
+
+    static void recurseDirectory(File currentDirectory, String[] args) {
+        if (currentDirectory.directory) {
+            File[] files = currentDirectory.listFiles()
+            files.each { currentFile ->
+                if(currentFile.isDirectory()) {
+                    recurseDirectory(currentFile, args)
+                } else {
+                    if(currentFile.getAbsolutePath().endsWith("." + args[1]))
+                        parseSignature(currentFile, args)
+                }
+            }
+        }
+    }
+
+    static void parseSignature(File codeFile, String[] args) {
+        print codeFile.getName() + ":  "
+        def lines = codeFile.readLines()
         lines.each {
             def currentLine = it
             char[] characterArray = currentLine.toCharArray()
             characterArray.each { foo ->
-                args.each {
-                    if (it == foo)
-                        print it
+                for (int i = 2; i < args.length; i++) {
+                    if (args[i] == foo)
+                        print foo
                 }
             }
         }
+        println()
     }
 
 }
