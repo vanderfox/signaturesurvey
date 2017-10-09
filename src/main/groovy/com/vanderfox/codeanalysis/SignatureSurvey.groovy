425d3364eeb2c82054e8426d8a31f2a3cdd75472
Ryan Vanderwerf <vanderwerfr@ociweb.com>
2017-10-08 22:16:05 -0500

    add run task

--- src/main/groovy/com/vanderfox/codeanalysis/SignatureSurvey.groovy
+++ src/main/groovy/com/vanderfox/codeanalysis/SignatureSurvey.groovy
@@ -3,10 +3,10 @@ package com.vanderfox.codeanalysis
 /**
  * Created by lfox on 9/5/17.
  */
-class signaturesurvey {
+class SignatureSurvey {
 
     static void main(String[] args) {
-        File file = new File("/Users/lfox/HelloWorld.java")
+        File file = new File(args[0])
         def lines = file.readLines()
         lines.each {
             def currentLine = it
